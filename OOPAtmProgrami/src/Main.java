040a5673611e09d09dd45b4dfb5919e6bc634f57
Edanuur <100590628+Edanuur@users.noreply.github.com>
2022-02-28 17:12:51 +0300

    Add files via upload

---
+++ OOPAtmProgrami/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+	
+    public static void main(String[] args) {
+        
+        Atm atm = new Atm();
+        
+        Hesap hesap = new Hesap("Eda Ünal","12345", 2000);
+        
+        atm.calis(hesap);
+        System.out.println("Programdan Çýkýlýyor...");
+        
+            
+        
+    }
+ 
+}
